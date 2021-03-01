78eef79e5d524a621d005f2f1bd3af41ca6b348d
victormoya <vmoyacueto@gmail.com>
2021-03-01 19:52:17 +0100

    Muestra texto descripcion de imagenes

--- src/components/figure.js
+++ src/components/figure.js
@@ -1,12 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 //import Img from "gatsby-image"
 
+import { lorem } from "static/content.json"
+
 const Figure = ({ src, alt }) => {
+  const [showCaption, setShowCaption] = useState()
   return (
     <>
-      <div className="figure">
+      <div className="figure" onClick={() => setShowCaption(true)}>
         <img src={src} alt={alt} />
       </div>
+      {showCaption && (
+        <div className="caption">
+          <div onClick={() => setShowCaption(false)} className="close" />
+          <p>{lorem}</p>
+        </div>
+      )}
     </>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
-import Caption from "./caption"
 import Figure from "./figure"
 import Layout from "./layout"
 import Nav from "./nav"
 import Section from "./section"
 import Topbar from "./topbar"
 
-export { Caption, Figure, Layout, Nav, Section, Topbar }
+export { Figure, Layout, Nav, Section, Topbar }
